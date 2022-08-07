964ee74b4146b1dcf8cee7614f42638f0ce0186a
MaxLisniak <maxim.l.forstudy@gmail.com>
2022-08-07 18:15:38 +0300

    design improvement

---
+++ src/features/movies/NoPoster.tsx
@@ -0,0 +1,9 @@
+import "./NoPoster.scss";
+
+export const NoPoster = (props: { fontSize: number }) => {
+  return (
+    <div className="no-poster" >
+      <span>Without Poster</span>
+    </div>
+  )
+}
